4c5f7f61de5a4eef7fd27e162ce6d4463e295b5b
meverhart <mark.a.everhart@gmail.com>
2013-09-17 13:29:49 -0400

    fixed dynamic home page

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -5,16 +5,19 @@ class Owner
 	end
 
 	def birthdate
-		birthdate = Date.new(1990, 9 22)
+		birthdate = Date.new(1990, 9, 22)
 	end
 
 	def countdown
+		Rails.logger.debug 'DEBUG: entering Owner countdown method'
 		today = Date.today
 		birthday = Date.new(today.year, birthdate.month, birthdate.day)
 		if birthday > today
-			countdown = (bithday - today).to_i
+			countdown = (birthday - today).to_i
 		else
 			countdown = (birthday.next_year - today).to_i
+		end
 	end
+
 
 end
